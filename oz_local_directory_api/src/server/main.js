2571c4bcd13d3b50cf265179cc0a0801accb159a
Skillup Labs <nabin@skilluplabs.com.au>
2025-03-17 21:03:07 +1100

    Add  api end point

--- oz_local_directory_api/src/server/main.js
+++ oz_local_directory_api/src/server/main.js
@@ -1,7 +1,12 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import bodyParser from 'body-parser';
+
+import jobRouter from './routes/job.js';
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.get("/hello", (req, res) => {
   res.send("Hello Vite + React!");
@@ -10,9 +15,53 @@ app.get("/hello", (req, res) => {
 
 app.post("/register", (req, res) => {
   const data = req.body;
-  console.log(data);
-  res.json({data});
+
+  res.json({"status": "OK", message: `Successfully registered email: ${data.email}`});
 });
+
+
+app.post("/send-code", (req, res) => {
+  const body = req.body;
+  const email = body.email;
+  const code = "12345";
+  console.log({email,  code });
+  res.status(200).json({status: "OK", message: `Code sent to email: ${email}`});
+});
+
+
+app.post("/login", (req, res) => {
+  try {
+    const body = req.body;
+    res.status(200).json({status: "OK", message: `Login successful for email: ${body.email}`});
+  }
+  catch(error) {
+    res.status(500).json({status: "error", message: "Something went wrong, please try again!", error: error.message});
+  }
+});
+
+app.post("/forgot-password", (req, res) => {
+  try {
+    const body = req.body;
+    res.status(200).json({status: "OK", message: `Set new password for email: ${body.email}`});
+  }
+  catch(error) {
+    res.status(500).json({status: "error", message: "Something went wrong, please try again!", error: error.message});
+  }
+});
+
+app.post("/change-password", (req, res) => {
+  try {
+    const body = req.body;
+    res.status(200).json({status: "OK", message: `New password set to email: ${body.email}`});
+  }
+  catch(error) {
+    res.status(500).json({status: "error", message: "Something went wrong, please try again!", error: error.message});
+  }
+});
+
+
+app.use("/jobs", jobRouter)
+
 
 
 ViteExpress.listen(app, 3000, () =>
---
+++ oz_local_directory_api/src/server/routes/job.js
@@ -0,0 +1,27 @@
+
+import express from 'express';
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.send("All jobs");
+  });
+
+router.get("/:id", (req, res) => {
+ const id = req.params.id;
+ res.json({status: "OK", data: { id }});
+});
+
+router.post("/", (req, res) => {
+    const body = req.body;
+    res.json({status: "OK", message: "New job added successfully!", data: body});
+  });
+
+router.put("/:id", (req, res) => {
+    res.json({status: "OK", message: `Job with id ${req.params.id} updated successfully!`, data: body});
+});
+
+router.delete("/:id", (req, res) => {
+    res.json({status: "OK", message: `Job with id ${req.params.id} deleted successfully!`, data: body});
+});
+
+export default router;
